ea78a0faad668d5f898abe4d185d0748c426a79f
Mukhbit <w7core@gmail.com>
2025-08-23 00:48:44 +0500

    Initial commit

--- docs/workers-site/index.js
+++ docs/workers-site/index.js
@@ -1,61 +1,29 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+const assetManifest = JSON.parse(manifestJSON);
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return more error details in the response, which
- *    will help you debug issues with your worker.
- */
-const DEBUG = false
-
-addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
+// This is the modern ES Module format for a Cloudflare Worker
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      // getAssetFromKV is the function that serves your static files
+      return await getAssetFromKV(
+        {
+          request,
+          waitUntil: ctx.waitUntil.bind(ctx),
+        },
+        {
+          ASSET_NAMESPACE: env.__STATIC_CONTENT,
+          ASSET_MANIFEST: assetManifest,
+        }
+      );
+    } catch (e) {
+      // Handle cases where the file isn't found
+      let pathname = new URL(request.url).pathname;
+      return new Response(`"${pathname}" not found`, {
+        status: 404,
+        statusText: 'not found',
+      });
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
-  }
-})
-
-async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
-
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = req => new Request(url.toString(), req)
-
-  try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      }
-    }
-    return await getAssetFromKV(event, options)
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e2) {
-        // if we can't find a 404.html, throw the original error
-      }
-    }
-
-    return new Response(e.message || e.toString(), { status: 500 })
-  }
-}
+  },
+};
